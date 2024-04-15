77a2c7523cf8d790aad2a17229c8c57a4814b3a7
shubhrajyotiAqb <shubhrajyoti.m@aqbsolutions.com>
2024-04-15 14:45:06 +0530

    changes

--- resources/views/pages/web/home.blade.php
+++ resources/views/pages/web/home.blade.php
@@ -6,7 +6,7 @@
     <br/><br/><br/>
     <img src="{{asset('assets/img/web/home_page_banner1.jpg')}}" alt=""  width="100%">
     <h1 class='text-danger'>My Demo School</h1>
-    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+    <p class="lead">Use this document as a way to quickly start any new project using bootstrap.<br> All you get is this text and a mostly barebones HTML document.</p>
 </div>
 
 
